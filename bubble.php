f9f20471b5475538556869cb64042bfc6ce7cd37
dsl-group <103767033+dsl-group@users.noreply.github.com>
2024-02-06 17:32:12 +0200

    lesson 14

---
+++ bubble.php
@@ -0,0 +1,48 @@
+<?php
+
+function showArray($array) {
+    echo implode(',', $array) . PHP_EOL;
+}
+
+function sortUsingBubble($array, $n) {
+    if ($n == 1) {
+        return $array;
+    }
+
+    for ($i = 0; $i < $n - 1; $i++) {
+        if ($array[$i] > $array[$i + 1]) {
+            list($array[$i], $array[$i + 1]) = array($array[$i + 1], $array[$i]);
+            showArray($array);
+        }
+    }
+
+    return sortUsingBubble($array, $n - 1);
+}
+
+$array = [10, 7, 1, 4, 5, 9];
+
+$result = sortUsingBubble($array, count($array));
+
+//-- Результат --//
+// 7,10,1,4,5,9
+// 7,1,10,4,5,9
+// 7,1,4,10,5,9
+// 7,1,4,5,10,9
+// 7,1,4,5,9,10
+// 1,7,4,5,9,10
+// 1,4,7,5,9,10
+// 1,4,5,7,9,10
+
+//-- Пошук найменшого числа --//
+// Простіше за все після сортування вивести перше число з масиву,
+// але якщо обробляти початковий массив можна зробити порівняння так
+
+$minNumber = $array[0];
+
+foreach ($array as $number) {
+    if ($number < $minNumber) {
+        $minNumber = $number;
+    }
+}
+
+echo "Найменше число: " . $minNumber . "\n";
